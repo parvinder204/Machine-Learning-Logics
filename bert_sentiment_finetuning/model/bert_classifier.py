7ace023c0cd8a875fad2054902ffc9ed8f20a531
Parvinder Singh <techpinda1214@gmail.com>
2026-03-12 22:52:23 +0530

    model

---
+++ bert_sentiment_finetuning/model/bert_classifier.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+from transformers import BertModel
+
+
+class BertClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
+        self.dropout = nn.Dropout(0.3)
+        self.classifier = nn.Linear(768, 2)
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        pooled_output = outputs.pooler_output
+        x = self.dropout(pooled_output)
+        logits = self.classifier(x)
+        return logits
